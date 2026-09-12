ed348f9c2da1a5b89c8456ac820c5bc83313efc3
Himanshi Rangwani <himsrangwani010@gmail.com>
2026-09-12 18:46:20 +0530

    Time: 51 ms (62.47%), Space: 30 MB (5.45%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        freq={}
+        for i in range(len(nums)):
+            if nums[i] in freq:
+                nam=nums[i]
+                if abs(i-freq[nums[i]])<=k:
+                    return True
+            
+            freq[nums[i]]=i
+        return False                
+        
