17489661016781927987adb9ec5dfb0d519191e5
Himanshi Rangwani <himsrangwani010@gmail.com>
2026-09-12 16:01:16 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.79%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -3,11 +3,20 @@ class Solution(object):
         """
         :type digits: List[int]
         :rtype: List[int]
-        """ 
+         
         final=[]
         output=int("".join(map(str,digits)))
         output+=1
         final=list(map(int,str(output)))
-        return final
+        return final"""
+        i = len(digits)-1
+        while i >= 0:
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            else:
+                digits[i]=0
+                i-=1
+        return [1]+digits           
             
         
