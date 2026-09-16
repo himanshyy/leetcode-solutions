d17218723411476dbbcae28e84fe24eb4448c106
Himanshi Rangwani <himsrangwani010@gmail.com>
2026-09-16 15:25:44 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.5%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s)!=len(goal):
+            return False
+        if goal in s+s:
+            return True
+        return False    
+
+        
